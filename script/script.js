e62883127cea1f4088199284f2bc1f850afba8ba
daro-hub <128579506+daro-hub@users.noreply.github.com>
2024-12-03 17:52:55 +0000

    1

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.replace("index.html")
+}
+
+function products() {
+    window.location.replace("products.html")
+}
+
+function login() {
+    window.location.replace("login.html")
+}
